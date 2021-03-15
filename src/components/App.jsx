14730d29c39f233fde1032f91178201816c5add2
karthick <karthylock@gmail.com>
2021-03-15 19:09:02 +0530

    added delete method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,30 +5,37 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
 
-const [notes, setNotes] = useState([]);
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
 
-function addNote(newNote){
-setNotes(prevNotes => {
-return [...prevNotes, newNote];
-})
- 
-}
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
 
   return (
     <div>
       <Header />
-      <CreateArea 
-      onAdd={addNote}
-      />
-{notes.map((noteItem)=>{
-return <Note 
-title={noteItem.title}
-content={noteItem.content}
-
-/>
-})}
-      {/* <Note key={1} title="Note title" content="Note content" /> */}
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
